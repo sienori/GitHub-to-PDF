77ea89b6e782e1318369b5e115e3b6d122c36657
sienori <iilll7d6f@yahoo.co.jp>
2018-05-14 23:54:08 +0900

    Fix import error

--- src/content/main.js
+++ src/content/main.js
@@ -3,14 +3,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import Styles from 'styles.js';
-import createNewStylesWithUrl from 'createNewStylesWithUrl.js';
+import createNewStylesWithUrl from './createNewStylesWithUrl';
 
 let styles;
 browser.runtime.onMessage.addListener(request => {
     switch (request.message) {
         case 'changeStyle':
-            styles = createNewStylesWithUrl();
+            styles = new createNewStylesWithUrl();
             styles.changeStyle();
             break;
         case 'undoStyle':
